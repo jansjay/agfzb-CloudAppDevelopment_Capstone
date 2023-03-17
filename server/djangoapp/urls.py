c43097342e7238b9f24551e364f24827e9b297db
jansjay <sanjaya.jayasooriya@gmail.com>
2023-03-17 15:09:25 -0400

    Authentication

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,14 +11,19 @@ urlpatterns = [
 
     # path for about view
     path(route='contact', view=views.get_contact, name='contact'),
+    
     # path for contact us view
     path(route='about', view=views.get_about, name='about'),
+
     # path for registration
+    path(route='registration', view=views.registration_request, name='registration'),
 
     # path for login
-
+    path(route='login', view=views.login_request, name='login'),
+    
     # path for logout
-
+    path(route='logout', view=views.logout_request, name='logout'),
+    
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
