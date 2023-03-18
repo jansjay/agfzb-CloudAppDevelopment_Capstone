2a2599d70edb58b271b53660f0fac34386602961
jansjay <sanjaya.jayasooriya@gmail.com>
2023-03-18 14:32:33 -0400

    Models, Rest corrections

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,35 +1,35 @@
+import os
 import requests
 import json
 # import related models here
+from .models import CarDealer, DealerReview
 from requests.auth import HTTPBasicAuth
-from rest_framework import status
-from rest_framework.response import Response
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-watson_api_key = os.getenv['WATSON_API_KEY']
-watson_url = os.getenv['WATSON_URL']
+watson_api_key = os.getenv('WATSON_API_KEY')
+watson_url = os.getenv('WATSON_URL')
 #HTTPBasicAuth('apikey', api_key)
 # Create a `get_request` to make HTTP GET requests
 # e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
 #                                     auth=HTTPBasicAuth('apikey', api_key))
-def get_request(url, params=params, headers={'Content-Type': 'application/json'}, auth=None):
+def get_request(url, params, headers={'Content-Type': 'application/json'}, auth=None):
     try:
         response = requests.get(url, headers=headers, params=params, auth=auth)
+        return json.loads(response.text)
     except Exception as e:
         print("Exception occurred {}".format(e))
-        return Response({error: e}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-    
-    status_code = response.status_code
-    return Response(json.loads(response.text), status=response.status_code)
+        return {"error": e}
 
 # Create a `post_request` to make HTTP POST requests
 # e.g., response = requests.post(url, params=kwargs, json=payload)
-def post_request(url, params=kwargs, json=payload):
+def post_request(url, params, jsonLoad):
     try:
-        response = requests.post(url, params=kwargs, json=json)
-        return Response(json.loads(response.text), status=response.status_code)
+        response = requests.post(url, params, jsonLoad)
+        return json.loads(response.text)
     except Exception as e:
         print("Exception occurred {}".format(e))
-        return Response({error: e}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {"error": e}
 
 # Create a get_dealers_from_cf method to get dealers from a cloud function
 # def get_dealers_from_cf(url, **kwargs):
@@ -39,7 +39,7 @@ def get_dealers_from_cf(url, **kwargs):
     results = []
     json_result = get_request(url, params=kwargs)
     if json_result:
-        dealers = json_result
+        dealerships = json_result
         for dealership in dealerships:
             dealer = CarDealer(id=dealership["id"], state=dealership["state"], st=dealership["st"], 
                                    address=dealership["address"], zip=dealership["zip"], lat=dealership["lat"],
@@ -53,15 +53,13 @@ def get_dealers_from_cf(url, **kwargs):
 # - Parse JSON results into a DealerView object list
 def get_dealer_reviews_from_cf(url, dealerId):
     results = []
-    json_result = get_request(url, params={dealership: dealerId})
+    json_result = get_request(url, params={"dealership": dealerId})
     if json_result:
         reviews = json_result
         for review in reviews:
-            id, name, dealership, purchase, review, purchase_date, car_make, car_model, car_year, sentiment
-            results.append( DealerReview(id=reviews["id"], state=name["name"], dealership=reviews["dealership"], 
+            results.append( DealerReview(id=reviews["id"], state=reviews["name"], dealership=reviews["dealership"], 
                                    purchase=reviews["purchase"], review=reviews["review"], purchase_date=reviews["purchase_date"],
-                                   car_make=reviews["car_make"], car_model=reviews["car_model"], car_year=reviews["car_year"], analyze_review_sentiments(reviews["review"]))
-            results.append(dealer)
+                                   car_make=reviews["car_make"], car_model=reviews["car_model"], car_year=reviews["car_year"], sentiment=analyze_review_sentiments(reviews["review"])))
     return results
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
